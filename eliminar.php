bae9f06ad921c5f932a49f1d66800fa25c626196
derx2201 <dennizro@gmail.com>
2018-11-01 20:21:08 -0600

    creamos clase para eliminar datos

---
+++ eliminar.php
@@ -0,0 +1,14 @@
+<?php
+
+include("config.php");
+
+
+$id = $_GET['id'];
+
+$sql = "DELETE FROM users WHERE id=:id";
+$query = $dbConn->prepare($sql);
+$query->execute(array(':id' => $id));
+
+
+header("Location:listar.php");
+?>
