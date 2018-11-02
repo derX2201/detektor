21ce47be2af0ef54e08d7716c1b4a66757a731e6
derx2201 <dennizro@gmail.com>
2018-11-01 20:22:41 -0600

    agregamos interfaz de graficos para conteo de registros

---
+++ index.php
@@ -0,0 +1,466 @@
+<?php
+
+include_once("config.php");
+
+
+$result = $dbConn->query("SELECT COUNT(*) as total FROM users");
+
+$resulttotal = 0;
+
+while($row = $result->fetch(PDO::FETCH_ASSOC)) { 	
+    
+    $resulttotal = $row['total'];
+   
+  }
+
+
+
+
+?>
+ 
+ 
+ <!DOCTYPE html>
+<html lang="en" class="no-js">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="msapplication-tap-highlight" content="no" />
+
+  <title>Ing. Dennis E. Rodriguez</title>
+
+
+  <link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.min.css">
+  <link rel="stylesheet" href="bower_components/font-awesome/css/font-awesome.min.css">
+  <link rel="stylesheet" href="bower_components/mdi/css/materialdesignicons.min.css">
+  <link rel="stylesheet" href="bower_components/metisMenu/dist/metisMenu.min.css">
+  <link rel="stylesheet" href="bower_components/Waves/dist/waves.min.css"> 
+  <link rel="stylesheet" href="bower_components/malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.css"> 
+
+  <link rel="stylesheet" href="js/selects/cs-select.css">
+  <link rel="stylesheet" href="js/selects/cs-skin-elastic.css">
+
+  <link rel="stylesheet" href="bower_components/c3/c3.min.css">
+  <link rel="stylesheet" href="bower_components/zabuto_calendar/zabuto_calendar.min.css">
+    <script src="js/menu/modernizr.custom.js"></script>
+ 
+  <link rel="stylesheet" href="css/style.css">
+ 
+  <link rel="icon" href="img/favicon.ico" type="image/x-icon" />
+  <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon" />
+
+</head>
+
+<body>
+  <!--Preloader-->
+  <div id="preloader" class="preloader table-wrapper">
+    <div class="table-row">
+      <div class="table-cell">
+        <div class="la-ball-scale-multiple la-3x">
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div id="main-wrapper" class="main-wrapper">
+
+    <ul id="gn-menu" class="gn-menu-main">
+      <li class="gn-trigger">
+        <a id="menu-toggle" class="menu-toggle gn-icon gn-icon-menu">
+          <div class="hamburger">
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+          <div class="cross">
+            <span></span>
+            <span></span>
+          </div>
+        </a>
+        <nav class="gn-menu-wrapper">
+          <div class="gn-scroller">
+            <ul class="gn-menu metismenu">
+              <li class="gn-search-item">
+                <input placeholder="Buscar" type="search" class="gn-search">
+                <a class="search-icon"><i class="fa fa-search"></i><span>Buscar</span></a>
+              </li>
+              <li class="active">
+                <a href="index.html" aria-expanded="true"><i class="fa fa-th"></i>Estadisticas<span class="fa arrow"></span></a>
+                <ul class="gn-submenu collapse" aria-expanded="true">
+                  <li><a class="active" href="index.html">Graficos</a></li>
+                 
+                </ul>
+              </li>
+
+
+            </ul>
+          </div>
+          <!-- /gn-scroller -->
+          <div class="bottom-bnts">
+            
+            <a class="fix-nav" href="#"><i class="mdi mdi-pin"></i></a>
+            
+          </div>
+        </nav>
+      </li>
+      <li>
+        <a href="index.html" class="logo">Detektor - Ing. Dennis E. Rodriguez<i class="fa fa-toggle-on"></i></a>
+      </li>
+      
+      <li class="pull-right">
+        <ul class="nav navbar-right right-menu">
+          <li class="members-btn">
+            <a class="show-members">
+              <i class="fa fa-group"></i>
+            </a>
+          </li>
+         
+          <li class="dropdown notifications">
+            <a class="dropdown-toggle" data-toggle="dropdown">
+              <i class="fa fa-bell-o"></i> <span class="label label-warning"></span>
+            </a>
+            <ul class="dropdown-menu">
+              <li>
+                <h4 class="zero-m text-center">Notificacion</h4>
+              </li>
+            
+            </ul>
+          </li>
+          <li class="dropdown some-btn">
+            <a class="fullscreen">
+              <i class="mdi mdi-fullscreen"></i>
+            </a>
+          </li>
+        </ul>
+      </li>
+    </ul>
+
+    <!--Contenido Ing. Dennis E. Rodriguez -->
+    <div id="content" class="content">
+
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="page-header">
+                    <h2>Graficos</h2>
+                    <ol class="breadcrumb">
+                        <li><a href="index.html">Inicio</a></li>
+                        <li>Estadisticas</li>
+                        <li class="active">Graficos</li>
+                    </ol>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-6">
+                <div class="content-box">
+                    <div class="content-title big-box i-block">
+                        <h4 class="zero-m">Grafico Barras</h4>
+                        <div class="content-tools i-block pull-right">
+                            <a class="repeat-btn">
+                                <i class="fa fa-repeat"></i>
+                            </a>
+                            <a class="dropdown-toggle" data-toggle="dropdown">
+                                <i class="fa fa-chevron-down"></i>
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a href="#">Opcion 1</a></li>
+                                <li><a href="#">Opcion 2</a></li>
+                            </ul>
+                            <a class="close-btn">
+                                <i class="fa fa-times"></i>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="clearfix"></div>
+                    <div class="big-box">
+                        <div class="chartjs-container">
+                            <canvas id="chart-bar" height="120"></canvas>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="content-box">
+                    <div class="content-title big-box i-block">
+                        <h4 class="zero-m">Grafico Lineal</h4>
+                        <div class="content-tools i-block pull-right">
+                            <a class="repeat-btn">
+                                <i class="fa fa-repeat"></i>
+                            </a>
+                            <a class="dropdown-toggle" data-toggle="dropdown">
+                                <i class="fa fa-chevron-down"></i>
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a href="#">Opcion 1</a></li>
+                                <li><a href="#">Opcion 2</a></li>
+                            </ul>
+                            <a class="close-btn">
+                                <i class="fa fa-times"></i>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="clearfix"></div>
+                    <div class="big-box">
+                        <div class="chartjs-container">
+                            <canvas id="chart-line" height="120"></canvas>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-6">
+                <div class="content-box">
+                    <div class="content-title big-box i-block">
+                        <h4 class="zero-m">Grafico Pastel</h4>
+                        <div class="content-tools i-block pull-right">
+                            <a class="repeat-btn">
+                                <i class="fa fa-repeat"></i>
+                            </a>
+                            <a class="dropdown-toggle" data-toggle="dropdown">
+                                <i class="fa fa-chevron-down"></i>
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a href="#">Opcion 1</a></li>
+                                <li><a href="#">Opcion 2</a></li>
+                            </ul>
+                            <a class="close-btn">
+                                <i class="fa fa-times"></i>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="clearfix"></div>
+                    <div class="big-box">
+                        <div class="chartjs-container text-center">
+                            <canvas id="chart-pie" height="298"></canvas>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="content-box">
+                    <div class="content-title big-box i-block">
+                        <h4 class="zero-m">Grafico Circular</h4>
+                        <div class="content-tools i-block pull-right">
+                            <a class="repeat-btn">
+                                <i class="fa fa-repeat"></i>
+                            </a>
+                            <a class="dropdown-toggle" data-toggle="dropdown">
+                                <i class="fa fa-chevron-down"></i>
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a href="#">Opcion 1</a></li>
+                                <li><a href="#">Opcion 2</a></li>
+                            </ul>
+                            <a class="close-btn">
+                                <i class="fa fa-times"></i>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="clearfix"></div>
+                    <div class="big-box">
+                        <div class="chartjs-container">
+                            <canvas id="chart-doughnut" height="120"></canvas>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    
+    
+   
+        <div class="footer">Demo Detektor - Ing. Dennis E. Rodriguez</div>
+
+    </div>
+
+    <!--notificacion-->
+    <div id="welcome"></div>
+
+  
+
+  </div>
+
+    <div class="login-modal modal fade">
+      <div class="table-wrapper">
+        <div class="table-row">
+          <div class="table-cell text-center">
+           <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+            <div class="login i-block">
+              <div class="content-box">
+                <div class="light-blue-bg biggest-box">
+
+                  <h1 class="zero-m text-uppercase">Welcome</h1>
+
+                </div>
+                <div class="big-box text-left login-form">
+                  <h4 class="text-center">Login</h4>
+                  <form>
+                    <div class="form-group">
+                      <input type="text" class="form-control material" id="login" placeholder="Username">
+                    </div>
+                    <div class="form-group">
+                      <input type="password" class="form-control material" id="password" placeholder="Password">
+                    </div>
+
+                    <button type="submit" class="btn btn-block btn-info text-uppercase waves">Login</button>
+
+                  </form>
+                  <a href="#" class="green i-block">Forgot password?</a>
+                  <p>Do not have an account?</p>
+                  <a class="btn btn-block btn-warning text-uppercase waves waves-button">Create an account</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+  </div>
+  <div class="logout-modal modal fade">
+      <div class="table-wrapper">
+        <div class="table-row">
+          <div class="table-cell text-center">
+            <div class="login i-block">
+              <div class="content-box">
+                <div class="light-blue-bg biggest-box">
+                  <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                  <h3 class="zero-m text-uppercase">Do you want to exit?</h3>
+                  <a href="#" class="i-block" data-dismiss="modal">Yes</a>
+                  <a href="#" class="i-block" data-dismiss="modal">No</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+  </div>
+
+
+    <!--Scripts-->
+  <script src="bower_components/jquery/dist/jquery.min.js"></script>
+  <script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+  <script src="bower_components/metisMenu/dist/metisMenu.min.js"></script>
+  <script src="bower_components/Waves/dist/waves.min.js"></script>
+  <script src="bower_components/moment/min/moment.min.js"></script>
+  <script src="bower_components/jquery.nicescroll/jquery.nicescroll.min.js"></script>
+  <script src="bower_components/slimScroll/jquery.slimscroll.min.js"></script>
+  <script src="bower_components/malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.js"></script>
+  <script src="bower_components/cta/dist/cta.min.js"></script>
+  
+  <!--Menu-->
+  <script src="js/menu/classie.js"></script>
+  <script src="js/menu/gnmenu.js"></script>
+
+
+  <script src="js/selects/selectFx.js"></script>
+
+  
+  <script src="bower_components/flot/jquery.flot.js"></script>
+  <script src="bower_components/flot/jquery.flot.resize.js"></script>
+  <script src="bower_components/flot.curvedlines/curvedLines.js"></script>
+  <script src="js/flot/jquery.flot.orderBars.js"></script>
+
+
+
+
+  <script src="js/notifications/notificationFx.js"></script>
+
+  <!--Chart.js-->
+  <script src="bower_components/Chart.js/Chart.min.js"></script>
+  <!-- <script src="js/chart-js/example.js"></script> -->
+
+
+  
+  <script src="js/common.js"></script>
+
+  <script>
+    $(function () {
+
+      
+      setTimeout(function () {
+        
+        var notification = new NotificationFx({
+          message: '<span>Bienvenido</span>',
+          layout: 'attached',
+          effect: 'bouncyflip',
+          ttl: 4000,
+          wrapper: document.getElementById("welcome"),
+          type: 'notice', 
+        });
+        notification.show();
+      }, 1200);
+
+
+
+
+      var randomScalingFactor = function(){ return Math.round(Math.random()*100)};
+
+   var barChartData = {
+    labels : ["Registros"],
+    datasets : [
+        {
+            fillColor : "rgba(220,220,220,0.5)",
+            strokeColor : "rgba(220,220,220,0.8)",
+            highlightFill: "rgba(220,220,220,0.75)",
+            highlightStroke: "rgba(220,220,220,1)",
+            data : <?=json_encode($resulttotal);?>
+        },
+        {
+            fillColor : "rgba(66, 179, 130,0.5)",
+            strokeColor : "rgba(66, 179, 130,0.8)",
+            highlightFill : "rgba(66, 179, 130,0.75)",
+            highlightStroke : "rgba(66, 179, 130,1)",
+            data : <?=json_encode($resulttotal);?>
+        }
+    ]
+
+}
+
+
+     window.onload = function(){
+
+var ctx = document.getElementById("chart-bar").getContext("2d");
+window.myBar = new Chart(ctx).Bar(barChartData, {
+    responsive : true
+});
+
+var ctx2 = document.getElementById("chart-line").getContext("2d");
+window.myLine = new Chart(ctx2).Line(lineChartData, {
+    responsive: true
+});
+
+var ctx3 = document.getElementById("chart-pie").getContext("2d");
+window.myPie = new Chart(ctx3).Pie(pieData);
+
+var ctx4 = document.getElementById("chart-doughnut").getContext("2d");
+window.myDoughnut = new Chart(ctx4).Doughnut(doughnutData, {responsive : true});
+
+var ctx5 = document.getElementById("chart-polar").getContext("2d");
+window.myPolarArea = new Chart(ctx5).PolarArea(polarData, {
+    responsive:true
+});
+
+window.myRadar = new  Chart(document.getElementById("chart-radar").getContext("2d")).Radar(radarChartData, {
+    responsive: true
+});
+}
+
+
+      
+    });
+
+
+     
+     
+
+ 
+
+  </script>
+
+
+</body>
+
+</html>
