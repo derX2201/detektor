2df8c1da4ac98afc91dcd0535608fcc469f8cfe3
derx2201 <dennizro@gmail.com>
2018-11-01 20:09:43 -0600

    clase para conexion base de datos

---
+++ config.php
@@ -0,0 +1,18 @@
+<?php
+
+$databaseHost = 'localhost';
+$databaseName = 'test';
+$databaseUsername = 'root';
+$databasePassword = 'root';
+
+try {
+	
+	$dbConn = new PDO("pgsql:host={$databaseHost};dbname={$databaseName}", $databaseUsername, $databasePassword);
+	
+	$dbConn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+} catch(PDOException $e) {
+	echo $e->getMessage();
+}
+ 
+?>
